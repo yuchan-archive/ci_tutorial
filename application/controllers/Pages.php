0e73cd1e3137d76b693502d600a7b50b0c71a500
Yusuke Ohashi <github@junkpiano.me>
2015-12-31 22:37:43 +0900

    added view.

---
+++ application/controllers/Pages.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Pages
+ *
+ * @author Yusuke
+ */
+class Pages extends CI_Controller {
+    //put your code here
+    public function view($page = 'home') {
+        if(!file_exists(APPPATH . '/views/pages/' . $page . '.php')) {
+            show_404();
+        }
+        
+        $data['title'] = ucfirst($page);
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/' . $page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
